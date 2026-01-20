6c2371f56753f611b836b0fdda5058c914971451
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-01-20 14:10:01 -0800

    outlines working mostly
    #21

--- Hell Hotel No Depth/Assets/_Andrew/Scripts/Interactions/InteractableController.cs
+++ Hell Hotel No Depth/Assets/_Andrew/Scripts/Interactions/InteractableController.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections.Generic;
 
 public class InteractableController : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private List<SpriteRenderer> OutlineObjects;
+
     [Header("Events")]
     [SerializeField] private UnityEvent<InteractableController> OnInteract;
 
     [Header("Debug")]
     [SerializeField] private bool Hovered;
-    [SerializeField] private int activeTasks = 0;
+    [SerializeField] private int ActiveTasks = 0;
 
     private void Start() => gameObject.layer = LayerMask.NameToLayer("Interactable");
 
     public void SetHovered(bool _hovered) => Hovered = _hovered;
+
+    private void Update()
+    {
+        foreach (var outlineObject in OutlineObjects)
+        {
+            outlineObject.material.SetFloat("_Outline", Hovered && ActiveTasks <= 0 ? 1 : 0);
+        }
+    }
 
     public void Interact()
     {
@@ -22,14 +34,14 @@ public class InteractableController : MonoBehaviour
             return;
         }
 
-        activeTasks = OnInteract.GetPersistentEventCount();
+        ActiveTasks = OnInteract.GetPersistentEventCount();
         OnInteract.Invoke(this);
     }
 
     public void EndInteraction()
     {
-        activeTasks--;
-        if (activeTasks <= 0) Finish();
+        ActiveTasks--;
+        if (ActiveTasks <= 0) Finish();
     }
 
     private void Finish()
