76d19dc7faebf14c645381f2eaea7604b26b58ce
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-01-28 12:55:57 -0800

    monster animator controller working
    #56

--- Hell Hotel No Depth/Assets/_Andrew/Scripts/Controllers/EnemyController.cs
+++ Hell Hotel No Depth/Assets/_Andrew/Scripts/Controllers/EnemyController.cs
@@ -4,8 +4,14 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     [Header("References")]
+    [SerializeField] private Animator _Animator;
     [SerializeField] private Transform Target;
     [SerializeField] private NavMeshAgent Agent;
+
+    [Header("Debug")]
+    [SerializeField] private float Direction;
+    [SerializeField] private bool Attacking;
+    [SerializeField] private Vector3 LastPosition;
 
     private void Start()
     {
@@ -16,5 +22,13 @@ public class EnemyController : MonoBehaviour
     private void Update()
     {
         Agent.SetDestination(Target.position);
+
+        Vector3 movement = transform.position - LastPosition;
+        Direction = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+
+        _Animator.SetFloat("direction", Direction);
+        _Animator.SetFloat("attacking", Attacking ? 1 : 0);
+
+        LastPosition = transform.position;
     }
 }
