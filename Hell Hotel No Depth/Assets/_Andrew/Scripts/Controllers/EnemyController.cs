5323c2f8140623a37238cd44a39b2299fcdc7438
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-01-28 16:39:35 -0800

    working on enemy
    #52

--- Hell Hotel No Depth/Assets/_Andrew/Scripts/Controllers/EnemyController.cs
+++ Hell Hotel No Depth/Assets/_Andrew/Scripts/Controllers/EnemyController.cs
@@ -8,27 +8,50 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private Transform Target;
     [SerializeField] private NavMeshAgent Agent;
 
+    [Header("Settings")]
+    [SerializeField] private bool TargetPlayer = true;
+    [SerializeField] private float AttackDistance = 1;
+    [SerializeField] private float AttackCooldown = 1;
+    [SerializeField] private float AttackDamage = 5;
+
     [Header("Debug")]
     [SerializeField] private float Direction;
-    [SerializeField] private bool Attacking;
+    [SerializeField] private float LastDirection;
+    [SerializeField] private bool Attack;
+    [SerializeField] private float AttackCounter;
     [SerializeField] private Vector3 LastPosition;
 
     private void Start()
     {
         Agent.updateRotation = false;
         Agent.updateUpAxis = false;
+
+        if(TargetPlayer) Target = PlayerController.Instance.GetPlayer();
     }
 
     private void Update()
     {
-        Agent.SetDestination(Target.position);
+        Attack = Vector2.Distance(Target.position, transform.position) <= AttackDistance;
+
+        if(Attack)
+        {
+            AttackCounter -= Time.deltaTime;
+            if(AttackCounter <= 0)
+            {
+                AttackCounter = AttackCooldown;
+                HealthController.Instance.TakeDamage(AttackDamage);
+            }
+        }
+
+        Agent.SetDestination(Attack ? transform.position : Target.position);
 
         Vector3 movement = transform.position - LastPosition;
         Direction = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
 
-        _Animator.SetFloat("direction", Direction);
-        _Animator.SetFloat("attacking", Attacking ? 1 : 0);
+        if(!Attack) _Animator.SetFloat("direction", Direction);
+        _Animator.SetFloat("attack", Attack ? 1 : 0);
 
         LastPosition = transform.position;
+        LastDirection = Direction;
     }
 }
